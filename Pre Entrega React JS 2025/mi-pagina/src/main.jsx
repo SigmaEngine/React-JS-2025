ad5cdded656167172b08f222c580d90c8bf96596
SigmaEngine <agust@DESKTOP-FK2EJ6C>
2025-11-03 17:16:52 -0300

    Cambios con el Fondo

--- Pre Entrega React JS 2025/mi-pagina/src/main.jsx
+++ Pre Entrega React JS 2025/mi-pagina/src/main.jsx
@@ -14,3 +14,11 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
